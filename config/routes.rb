40d1c81db8c3a10af9b74a1b3415eb51e13d70f1
Luke O'Malley <Lukeomalley95@gmail.com>
2019-07-30 14:05:37 -0400

    Converted logo to link to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'matches#index'
   resources :users, only: [:show]
-  
+
   # routes for liking and disliking a user
   post '/like-user/:liked_user', to: 'matches#liked', as: 'like_user'
   post '/dislike-user/:disliked_user', to: 'matches#disliked', as: 'dislike_user'
