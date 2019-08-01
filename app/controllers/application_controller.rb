0c2227f218eebd6564ad43440ab3a84093a219cf
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-01 18:31:51 -0400

    styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery prepend: true
-
-
-
-
+  
   protected
 
   def configure_permitted_parameters
